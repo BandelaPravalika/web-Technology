b652bf68160432dc90ea509a9056e45250c0307f
BandelaPravalika <bandelapravalika015@gmail.com>
2025-04-11 17:14:13 +0530

    MyFirst Commit

---
+++ JavaScript1/Demo.js
@@ -0,0 +1,11 @@
+
+    
+        let a=10;
+        let b=0o36;
+        let c=0x1e;
+        let d=0b11110;
+        document.write(a+"<br>");
+        document.write(b+"<br>");
+        document.write(c+"<br>");
+        document.write(d+"<br>");
+    
